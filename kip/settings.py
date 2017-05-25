35e21b0f8d6c27720afec017d21f795ee7feea8e
mehmet emin karakas <mkarakas@turksat.com.tr>
2017-05-25 11:34:18 +0300

    Language Settings

--- kip/settings.py
+++ kip/settings.py
@@ -115,11 +115,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'tr-TR'
 
 LANGUAGES = (
     ('en', _('English')),
     ('tr', _('Turkce')),
+    ('ru', _('Rus')),
 )
 
 LOCALE_PATHS = (
